21ec57cc9283db0aa1b02ee86a7aaa32f3e5c1b7
Sheraz Ali <Sherazalibk@gmail.com>
2018-08-10 16:30:13 -0700

    Two layout are created...

---
+++ app/src/main/java/com/sheraz/ali/weather_app/change_city.kt
@@ -0,0 +1,12 @@
+package com.sheraz.ali.weather_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class change_city : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_change_city)
+    }
+}
